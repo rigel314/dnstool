4832f1cdafe0a6ba60f02ba2b57c03b172572159
Cody Creager <pi.rubiks@gmail.com>
2018-05-12 23:09:14 -0700

    Add comments

--- dnstool.go
+++ dnstool.go
@@ -66,16 +66,19 @@ var cfg config
 var stats statistics
 
 func main() {
+	// Print line numbers
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	// Use all cores if you have to
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// load JSON config
+	// load hard coded defaults
 	err := json.Unmarshal(defaultjs, &cfg)
 	if(err != nil) {
 		log.Println(err)
 		log.Fatal("hard coded defaults caused error")
 	}
+	// load JSON config
 	cfgfp, err := os.Open("config.json")
 	if(err != nil) {
 		log.Print("config.json failed loading, using defaults")
@@ -84,22 +87,23 @@ func main() {
 		err = cfgjs.Decode(&cfg)
 		if(err != nil) {
 			log.Print(err)
-			log.Fatal("invalid config")
+			log.Fatal("invalid config") // Die if a parsing error happens
 		}
 	}
 
+	// filter out 127.0.0.1's from Server list
 	servfilt := cfg.Servers[:0]
 	for _, serv := range cfg.Servers {
 		if(bytes.Compare(net.ParseIP(serv), net.IPv4(127, 0, 0, 1)) != 0) {
 			servfilt = append(servfilt, serv)
 		}
 	}
+	cfg.Servers = servfilt
 
+	// initialize statistics
 	stats.ServerHits = make([]int, len(cfg.Servers))
 
-	// fmt.Println(cfg)
-
-	// Server
+	// DNS Server
 	conn, err := net.ListenPacket("udp4", "127.0.0.1:53") // TODO: config
 	if(err != nil) {
 		log.Fatal("Couldn't listen on 127.0.0.1:53")
@@ -130,8 +134,8 @@ func main() {
 		}
 	}()
 
+	// HTTP Server for serving configured 301 redirects
 	go func() {
-		// TODO: check errors
 		http.HandleFunc("/", redirector)
 		err := http.ListenAndServe("127.0.0.1:80", nil)
 		if(err != nil) {
@@ -155,7 +159,7 @@ func main() {
 		go func(addr net.Addr, buf []byte) {
 			// log.Printf("got %d bytes from %s", n, addr)
 
-			// TODO: inspect request and immediatly send CNAME/"hosts file" reply from configuration
+			// inspect request and immediately send CNAME/"hosts file" reply from configuration
 			q, ok := parseQuery(buf)
 			if(ok) {
 				// log.Print(q.name)
@@ -176,13 +180,15 @@ func main() {
 
 			fwChan := make(chan dnsResp)
 
+			// Wait for responses from forwarded DNS requests
 			go func() {
 				select{
-					case val, ok := <-fwChan:
+					case val, ok := <-fwChan: // Only care about the first reply
 						if(ok) {
 							// log.Printf("using dns from server %d\n", val.idx)
-							responseCh<- val
-							stats.ServerHits[val.idx]++
+							responseCh<- val // send response to response handler
+							stats.ServerHits[val.idx]++ // Update stats
+							// Print out stats every 100 forwarded requests
 							if(cfg.General.ShowStats && sumSlice(stats.ServerHits) % 100 == 0) {
 								log.Printf("DNS breakdown:\n")
 								log.Printf("\t#responses\tserver\n")
@@ -196,9 +202,11 @@ func main() {
 				}
 			}()
 
+			// Forward request to each configured server
 			for idx, srv := range cfg.Servers {
-				// Forward request to each configured server
+				// launch a goroutine to send requests in parallel
 				go func(idx int, srv string) {
+					// Make independent socket
 					rconn, err := net.ListenPacket("udp4", "")
 					if(err != nil) {
 						log.Print(err)
@@ -206,6 +214,7 @@ func main() {
 					}
 					defer rconn.Close()
 
+					// Send actual request
 					remoteAddr := net.UDPAddr{IP: net.ParseIP(srv), Port: 53}
 					_, err = rconn.WriteTo(buf[:n], net.Addr(&remoteAddr))
 					if(err != nil) {
@@ -227,11 +236,13 @@ func main() {
 
 					// TODO: parse response for NXdomain override
 
-					// Send response to client
+					// Create response and copy to smaller buffer
 					sm_rbuf := make([]byte, n)
 					copy(sm_rbuf, rbuf[:n])
 					resp := dnsResp{dest: addr, data: sm_rbuf, idx: idx}
 
+					// Send response to client
+					// non-blocking because we only care about the fastest response
 					select {
 						case fwChan<- resp:
 						default:
@@ -243,6 +254,7 @@ func main() {
 	}
 }
 
+// Serves 301 redirects, if configured
 func redirector(w http.ResponseWriter, req *http.Request) {
 	dest := req.Host + req.URL.String()
 	log.Println(req.Host)
@@ -258,6 +270,7 @@ func redirector(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, fmt.Sprintf("no redirect or reverse proxy config for %s\n", req.Host))
 }
 
+// Sum the elements of a slice of ints
 func sumSlice(s []int) int {
 	var sum int = 0
 	for _, v := range s {
