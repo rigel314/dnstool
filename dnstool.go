9d0a56d8590e1e5d5abfef3f3482b1c4c2f53ddc
Cody Creager <pi.rubiks@gmail.com>
2018-09-15 11:40:58 -0700

    Go fmt and respect more parameters

--- dnstool.go
+++ dnstool.go
@@ -7,46 +7,46 @@ package main
 // You can configure NXdomain overrides for stupid ISPs that intercept and replace unencrypted NXdomain responses with their own bullshit
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"time"
-	"os"
-	"io"
-	"runtime"
-	"bytes"
-	"strings"
 	"net/http"
-	"encoding/json"
+	"os"
+	"runtime"
+	"strings"
+	"time"
 )
 
 type config struct {
-	General genCfg
+	General         genCfg
 	HTTPlistenPorts []uint16
-	Servers []string
-	Hosts []host
-	Cnames []cname
-	Redirect301s []redirect
-	NXoverride []string
+	Servers         []string
+	Hosts           []host
+	Cnames          []cname
+	Redirect301s    []redirect
+	NXoverride      []string
 }
 type genCfg struct {
-	BindIP string
-	DNSPort int16
+	BindIP     string
+	DNSPort    int16
 	DNSTCPalso bool
-	TimeoutMs int
-	ShowStats bool
+	TimeoutMs  int
+	ShowStats  bool
 }
 type host struct {
-	IP string
+	IP   string
 	Name string
 }
 type cname struct {
-	Name string
+	Name  string
 	Cname string
 }
 type redirect struct {
 	From string
-	To string
+	To   string
 }
 
 type statistics struct {
@@ -56,7 +56,7 @@ type statistics struct {
 type dnsResp struct {
 	dest net.Addr
 	data []byte
-	idx int
+	idx  int
 }
 
 // Defaults
@@ -78,27 +78,31 @@ func main() {
 
 	// load hard coded defaults
 	err := json.Unmarshal(defaultjs, &cfg)
-	if(err != nil) {
+	if err != nil {
 		log.Println(err)
 		log.Fatal("hard coded defaults caused error")
 	}
 	// load JSON config
 	cfgfp, err := os.Open("config.json")
-	if(err != nil) {
+	if err != nil {
 		log.Print("config.json failed loading, using defaults")
 	} else {
 		cfgjs := json.NewDecoder(cfgfp)
 		err = cfgjs.Decode(&cfg)
-		if(err != nil) {
+		if err != nil {
 			log.Print(err)
 			log.Fatal("invalid config") // Die if a parsing error happens
 		}
 	}
 
+	if cfg.General.DNSTCPalso {
+		log.Println("ignoring unsupported config: DNSTCPalso = true")
+	}
+
 	// filter out 127.0.0.1's from Server list
 	servfilt := cfg.Servers[:0]
 	for _, serv := range cfg.Servers {
-		if(bytes.Compare(net.ParseIP(serv), net.IPv4(127, 0, 0, 1)) != 0) {
+		if bytes.Compare(net.ParseIP(serv), net.IPv4(127, 0, 0, 1)) != 0 {
 			servfilt = append(servfilt, serv)
 		}
 	}
@@ -108,9 +112,10 @@ func main() {
 	stats.ServerHits = make([]int, len(cfg.Servers))
 
 	// DNS Server
-	conn, err := net.ListenPacket("udp4", "127.0.0.1:53") // TODO: config
-	if(err != nil) {
-		log.Fatal("Couldn't listen on 127.0.0.1:53")
+	bindstr := fmt.Sprintf("%s:%d", cfg.General.BindIP, cfg.General.DNSPort)
+	conn, err := net.ListenPacket("udp4", bindstr)
+	if err != nil {
+		log.Fatal("Couldn't listen on" + bindstr)
 	}
 	defer conn.Close()
 
@@ -123,29 +128,31 @@ func main() {
 	go func() {
 		for {
 			// wait for response to be generated
-			resp, ok := <- responseCh
-			if(!ok) {
+			resp, ok := <-responseCh
+			if !ok {
 				log.Print(err)
 				continue
 			}
 
 			// Actually send response
 			_, err := conn.WriteTo(resp.data, resp.dest)
-			if(err != nil) {
+			if err != nil {
 				log.Print(err)
 				continue
 			}
 		}
 	}()
 
-	// HTTP Server for serving configured 301 redirects
-	go func() {
-		http.HandleFunc("/", redirector)
-		err := http.ListenAndServe("127.0.0.1:80", nil)
-		if(err != nil) {
-			log.Fatal(err)
-		}
-	}()
+	// HTTP Servers for serving configured 301 redirects
+	for _, port := range cfg.HTTPlistenPorts {
+		go func(port uint16) {
+			http.HandleFunc("/", redirector)
+			err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(port)
+	}
 
 	fmt.Printf("Server up\n")
 
@@ -154,7 +161,7 @@ func main() {
 		buf := make([]byte, 1024)
 
 		n, addr, err := conn.ReadFrom(buf)
-		if(err != nil) {
+		if err != nil {
 			log.Print(err)
 			continue
 		}
@@ -165,17 +172,17 @@ func main() {
 
 			// inspect request and immediately send CNAME/"hosts file" reply from configuration
 			q, ok := parseQuery(buf)
-			if(ok) {
+			if ok {
 				// log.Print(q.name)
 				for _, name := range cfg.Cnames {
-					if(strings.EqualFold(name.Name, q.name)) {
+					if strings.EqualFold(name.Name, q.name) {
 						// TODO: make CNAME replies also provide an A answer
 						responseCh <- dnsResp{dest: addr, data: genResponse(dnsResponse{id: q.id, name: q.name, cname: name.Cname, a: false})}
 						return
 					}
 				}
 				for _, name := range cfg.Hosts {
-					if(strings.EqualFold(name.Name, q.name)) {
+					if strings.EqualFold(name.Name, q.name) {
 						responseCh <- dnsResp{dest: addr, data: genResponse(dnsResponse{id: q.id, name: q.name, ip: name.IP, a: true})}
 						return
 					}
@@ -186,23 +193,23 @@ func main() {
 
 			// Wait for responses from forwarded DNS requests
 			go func() {
-				select{
-					case val, ok := <-fwChan: // Only care about the first reply
-						if(ok) {
-							// log.Printf("using dns from server %d\n", val.idx)
-							responseCh<- val // send response to response handler
-							stats.ServerHits[val.idx]++ // Update stats
-							// Print out stats every 100 forwarded requests
-							if(cfg.General.ShowStats && sumSlice(stats.ServerHits) % 100 == 0) {
-								log.Printf("DNS breakdown:\n")
-								log.Printf("\t#responses\tserver\n")
-								for i, v := range stats.ServerHits {
-									log.Printf("\t% 10d\t%s", v, cfg.Servers[i])
-								}
+				select {
+				case val, ok := <-fwChan: // Only care about the first reply
+					if ok {
+						// log.Printf("using dns from server %d\n", val.idx)
+						responseCh <- val           // send response to response handler
+						stats.ServerHits[val.idx]++ // Update stats
+						// Print out stats every 100 forwarded requests
+						if cfg.General.ShowStats && sumSlice(stats.ServerHits)%100 == 0 {
+							log.Printf("DNS breakdown:\n")
+							log.Printf("\t#responses\tserver\n")
+							for i, v := range stats.ServerHits {
+								log.Printf("\t% 10d\t%s", v, cfg.Servers[i])
 							}
 						}
-					case <-time.After(time.Duration(cfg.General.TimeoutMs) * time.Millisecond):
-						log.Println("timout")
+					}
+				case <-time.After(time.Duration(cfg.General.TimeoutMs) * time.Millisecond):
+					log.Println("timout")
 				}
 			}()
 
@@ -212,7 +219,7 @@ func main() {
 				go func(idx int, srv string) {
 					// Make independent socket
 					rconn, err := net.ListenPacket("udp4", "")
-					if(err != nil) {
+					if err != nil {
 						log.Print(err)
 						return
 					}
@@ -221,7 +228,7 @@ func main() {
 					// Send actual request
 					remoteAddr := net.UDPAddr{IP: net.ParseIP(srv), Port: 53}
 					_, err = rconn.WriteTo(buf[:n], net.Addr(&remoteAddr))
-					if(err != nil) {
+					if err != nil {
 						log.Print(err)
 						return
 					}
@@ -231,7 +238,7 @@ func main() {
 
 					rconn.SetReadDeadline(time.Now().Add(time.Duration(cfg.General.TimeoutMs) * time.Millisecond))
 					n, _, err := rconn.ReadFrom(rbuf)
-					if(err != nil) {
+					if err != nil {
 						log.Printf("err forwarding to %s", srv)
 						// TODO: actually, send an NXdomain on timeout
 						log.Print(err)
@@ -248,9 +255,9 @@ func main() {
 					// Send response to client
 					// non-blocking because we only care about the fastest response
 					select {
-						case fwChan<- resp:
-						default:
-							// log.Printf("chan %d not ready\n", idx)
+					case fwChan <- resp:
+					default:
+						// log.Printf("chan %d not ready\n", idx)
 					}
 				}(idx, srv)
 			}
@@ -263,7 +270,7 @@ func redirector(w http.ResponseWriter, req *http.Request) {
 	dest := req.Host + req.URL.String()
 	log.Println(req.Host)
 	for _, name := range cfg.Redirect301s {
-		if(strings.EqualFold(name.From, req.Host)) {
+		if strings.EqualFold(name.From, req.Host) {
 			newdest := "http://" + name.To + req.URL.String()
 			log.Printf("redirecting %s=>%s\n", dest, newdest)
 			http.Redirect(w, req, newdest, http.StatusMovedPermanently)
